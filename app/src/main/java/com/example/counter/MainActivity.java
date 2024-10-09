1f5e7b9c9b75a3e28f00f89fc372ea961ea5206f
gui-motas <monjogui@gmail.com>
2024-10-09 18:22:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.counter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView counterView;
+    private Button incrementButton;
+    private int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+            counterView = findViewById(R.id.counterView);
+            incrementButton = findViewById(R.id.button);
+
+
+            if (savedInstanceState != null) {
+                counter = savedInstanceState.getInt("counter");
+                counterView.setText(String.valueOf(counter));
+
+            } else {
+                counter = 0;  // Inicializa o contador
+            }
+
+            incrementButton.setOnClickListener(view -> {
+
+                counter++;
+                counterView.setText(String.valueOf(counter));
+
+            });
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+
+
+            });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("counter");
+        counterView.setText("" + counter);
+
+    }
+}
